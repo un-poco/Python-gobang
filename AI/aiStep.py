00804ff4a00bab275a8de70bb267520691205118
caiyu-2018 <1565759359@qq.com>
2020-11-06 17:56:13 +0800

    枚举法修改

--- AI/aiStep.py
+++ AI/aiStep.py
@@ -1,14 +1,14 @@
-# from search import *
-from module.search import *
+from AI.search import *
 import numpy as np
 
 
-def getScore(state, pos, key):
-    attack = 501 * live4(state, pos, key) + 31 * chong4(state, pos, key) + 31 * live3(state, pos, key) + 11 * mian3(
-        state, pos, key) + 51 * live2(state, pos, key)
-    key = -key
-    defend = 500 * live4(state, pos, key) + 30 * chong4(state, pos, key) + 30 * live3(state, pos, key) + 10 * mian3(
-        state, pos, key) + 501 * live2(state, pos, key)
+def getScore(state, pos):
+    key = 2
+    attack = 20000000 * finish5(state, pos, key) + 50001 * live4(state, pos, key) + 3001 * chong4(state, pos, key) + \
+             3001 * live3(state, pos, key) + 1001 * mian3(state, pos, key) + 501 * live2(state, pos, key)
+    key = 1
+    defend = 2000000 * finish5(state, pos, key) + 50000 * live4(state, pos, key) + 3000 * chong4(state, pos, key) + \
+             3000 * live3(state, pos, key) + 1000 * mian3(state, pos, key) + 500 * live2(state, pos, key)
     return attack + defend
 
 
@@ -17,7 +17,6 @@ def aiStep(state):
     state: (15, 15) 棋盘状态，1、-1分别为双方，0为空，-1表示电脑
     return: array([x, y])表示落子坐标
     """
-    key = -1
 
     opt_list = np.where(state == 0)  # 落子条件一：这个格子必须为空
     opt_list = [np.array([opt_list[0][i], opt_list[1][i]]) for i in range(len(opt_list[0]))]  # 每个坐标转换成ndarray
@@ -42,16 +41,16 @@ def aiStep(state):
         d += 1
 
     score_list = []
-    for opt in opt_list:  # 对每个可选位置打分
-        score_list.append(getScore(state, opt, key))
+    for i in range(len(opt_list)):  # 对每个可选位置打分
+        opt = opt_list[i]
+        score_list.append(getScore(state, opt))
 
     max_index = score_list.index(max(score_list))
-
     return opt_list[max_index]
 
 
 if __name__ == '__main__':
     board = np.zeros((15, 15))
     board[3, 3] = 1
-    res = aiStep(board)
+    res = live2(board, np.array([2, 2]), 1)
     print(res)
