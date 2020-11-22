45147e277faa75f650bec0f403f044db2e88504c
蔡煜 <1565759359@qq.com>
2020-11-22 08:37:19 +0800

    Add files via upload

--- GUI/Player2GUI.py
+++ GUI/Player2GUI.py
@@ -5,17 +5,32 @@
 '''
 import sys
 import time
+import os
 sys.path.append('D:/Git/PY_gobang/GUI')
 sys.path.append('D:/Git/PY_gobang/GUI/source')
 sys.path.append('D:/Git/PY_gobang/AI')
 from chessboard import ChessBoard
-from ai import searcher
-import MyButton
+from gobangGUI import GoBang
 import numpy as np
 # 客户端1代码
 import socket
 import threading
+import cgitb
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QIcon, QPalette, QPainter
+from PyQt5.QtMultimedia import QSound
+from PyQt5 import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+sys.path.append("D:/Pyfiles/PY_gobang/AI")
+sys.path.append('D:/Git/PY_gobang/GUI/double_play')
+import MyButton
+import gobangGUI
+import doublePlayerGUI
 
+recent_place = []
 # 棋盘基本参数等
 WIDTH = 760
 HEIGHT = 650
@@ -28,18 +43,6 @@ PIECE = 34
 EMPTY = 0
 BLACK = 1
 WHITE = 2
-
-import sys
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMessageBox
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QIcon, QPalette, QPainter
-from PyQt5.QtMultimedia import QSound
-from PyQt5 import *
-recent_place = []
-
-
-
 
 # ----------------------------------------------------------------------
 # 重新定义Label类
@@ -55,7 +58,7 @@ class LaBel(QLabel):
         e.ignore()
 
 
-class GoBang(QWidget):
+class Player2GoBang(GoBang):
     backSignal = QtCore.pyqtSignal()  # 返回信号，用来和主界面连接
     def __init__(self):
         super().__init__()
@@ -135,99 +138,6 @@ class GoBang(QWidget):
             except:
                 pass
 
-    def initUI(self):  # UI初始化
-        self.chessboard = ChessBoard()  # 棋盘类，详见chessboard.py
-
-        palette1 = QPalette()  # 设置棋盘背景
-        palette1.setBrush(self.backgroundRole(), QtGui.QBrush(QtGui.QPixmap('source/游戏界面1.png')))
-        self.setPalette(palette1)
-
-        # self.setCursor(Qt.PointingHandCursor)  # 鼠标变成手指形状
-        self.sound_piece = QSound("sound/move.wav")  # 加载落子音效
-        self.sound_win = QSound("sound/win.wav")  # 加载胜利音效
-        self.sound_defeated = QSound("sound/defeated.wav")  # 加载失败音效
-
-        self.resize(WIDTH, HEIGHT)  # 画布大小，设为固定值，不允许缩放
-        self.setMinimumSize(QtCore.QSize(WIDTH, HEIGHT))
-        self.setMaximumSize(QtCore.QSize(WIDTH, HEIGHT))
-
-        self.setWindowTitle("GoBang_WHITE")  # 窗口名称
-        self.setWindowIcon(QIcon('source/icon.ico'))  # 窗口图标
-
-        # 所有按钮的图标和布局
-        self.backBtn = MyButton.MyButton('source/返回按钮_hover.png',
-                                         'source/返回按钮_normal.png',
-                                         'source/返回按钮_press.png',
-                                         parent=self)
-        self.backBtn.move(610, 80)
-
-        self.startBtn = MyButton.MyButton('source/开始按钮_hover.png',
-                                          'source/开始按钮_normal.png',
-                                          'source/开始按钮_press.png',
-                                          parent=self)
-        self.startBtn.move(610, 180)
-
-        self.returnBtn = MyButton.MyButton('source/悔棋按钮_hover.png',
-                                           'source/悔棋按钮_normal.png',
-                                           'source/悔棋按钮_press.png',
-                                           parent=self)
-        self.returnBtn.move(610, 400)
-
-        self.loseBtn = MyButton.MyButton('source/认输按钮_hover.png',
-                                         'source/认输按钮_normal.png',
-                                         'source/认输按钮_press.png',
-                                         parent=self)
-        self.loseBtn.move(610, 500)
-
-        # 绑定按钮
-        self.backBtn.clicked.connect(self.goBack)
-        self.startBtn.clicked.connect(self.restart)
-        self.loseBtn.clicked.connect(self.lose)
-        self.returnBtn.clicked.connect(self.returnOneStep)
-
-        # self.gameStatu = []
-
-        self.black = QPixmap('source/black.png')  # 黑白棋子
-        self.white = QPixmap('source/white.png')
-
-        self.piece_now = BLACK
-
-
-
-        self.step = 0 # 步数
-        self.x, self.y = 1000, 1000
-
-
-
-        self.pieces = [LaBel(self) for i in range(225)]  # 新建棋子标签，准备在棋盘上绘制棋子
-        for piece in self.pieces:
-            piece.setVisible(True)  # 图片可视
-            piece.setScaledContents(True)  # 图片大小根据标签大小可变
-
-        # self.mouse_point.raise_()  # 鼠标始终在最上层
-        self.ai_down = False  # 主要是为了加锁，当值是False的时候在等待对方下棋，这时候己方鼠标点击失效，要忽略掉 mousePressEvent
-
-        self.setMouseTracking(True)
-        self.show()
-
-    # 返回键设计，回到主菜单
-    def goBack(self):
-        self.backSignal.emit()
-        self.close()
-
-    def closeEvent(self, a0: QtGui.QCloseEvent):
-        self.backSignal.emit()
-
-    def paintEvent(self, event):  # 画出指示箭头
-        qp = QPainter()
-        qp.begin(self)
-        self.drawLines(qp)
-        qp.end()
-
-    # def mouseMoveEvent(self, e):  # 黑色棋子随鼠标移动
-    #     # self.lb1.setText(str(e.x()) + ' ' + str(e.y()))
-    #     self.mouse_point.move(e.x() - 16, e.y() - 16)
-
     def mousePressEvent(self, e):  # 玩家2下棋
 
         if e.button() == Qt.LeftButton and self.ai_down:
@@ -242,15 +152,6 @@ class GoBang(QWidget):
                     self.draw(i, j)
                     recent_place.append([i, j, WHITE])
                     self.ai_down = False # 加锁 避免鼠标再点击
-
-
-    def drawLines(self, qp):  # 绘制lines
-        if self.step != 0:
-            pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
-            qp.setPen(pen)
-            qp.drawLine(self.x - 5, self.y - 5, self.x + 3, self.y + 3)
-            qp.drawLine(self.x + 3, self.y, self.x + 3, self.y + 3)
-            qp.drawLine(self.x, self.y + 3, self.x + 3, self.y + 3)
 
     # 落子代码
     def draw(self, i, j):
@@ -280,31 +181,6 @@ class GoBang(QWidget):
             # self.mouse_point.clear()
             self.gameover(winner)
 
-    def coordinate_transform_map2pixel(self, i, j):
-        # 从 chessMap 里的逻辑坐标到 UI 上的绘制坐标的转换
-        return MARGINXL + j * GRID - PIECE / 2, MARGINY + i * GRID - PIECE / 2
-
-    def coordinate_transform_pixel2map(self, x, y):
-        # 从 UI 上的绘制坐标到 chessMap 里的逻辑坐标的转换
-        i, j = int(round((y - MARGINY) / GRID)), int(round((x - MARGINXL) / GRID))
-        # 有MAGIN, 排除边缘位置导致 i,j 越界
-        if i < 0 or i >= 15 or j < 0 or j >= 15:
-            return None, None
-        else:
-            return i, j
-
-    # 这块代码后期还可以再改，用图片做出来应该会更好看
-    def gameover(self, winner):
-        if winner == BLACK:
-            self.sound_win.play()
-            reply = QMessageBox.question(self, 'You Lost!', 'Continue?',
-                                         QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-        else:
-            self.sound_defeated.play()
-            reply = QMessageBox.question(self, 'You Win!', 'Continue?',
-                                         QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-
-
     # 认输功能键，不知道为什么卡的厉害。人机对战的认输还没写
     def lose(self):
         self.c.send('r'.encode())
@@ -314,7 +190,7 @@ class GoBang(QWidget):
         self.close()
 
 
-    # 重开，这个问题有点大，重新绘图我没实现。目前是把数组清空了，图没变(在上面重新画棋盘也太蠢了吧，刷新界面会比较好但是我没写出来:/)
+    # 重开
     def restart(self):
         self.huiqi_flag = True
         self.piece_now = BLACK
@@ -387,7 +263,7 @@ class Mainwindow(QWidget):
 
     def startDoubleGame(self):
         # 构建双人对战界面
-        self.doublePlayerGame = GoBang()
+        self.doublePlayerGame = Player2GoBang()
         # 绑定返回界面
         self.doublePlayerGame.backSignal.connect(self.showStartGame)
 
@@ -410,24 +286,12 @@ class Mainwindow(QWidget):
         self.show()
         self.SingleGame.close()
 
-
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-import sys
-import os
-sys.path.append("D:/Pyfiles/PY_gobang/AI")
-sys.path.append('D:/Git/PY_gobang/GUI/double_play')
-import MyButton
-import gobangGUI
-import doublePlayerGUI
-
 if __name__ == '__main__':
     '''
     app = QApplication(sys.argv)
     ex = GoBang()
     sys.exit(app.exec_())
     '''
-    import cgitb
 
     cgitb.enable("text")
     a = QApplication(sys.argv)
